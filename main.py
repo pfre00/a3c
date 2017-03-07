255d6a37754bb1bdffad5a6688198f3928b032c3
Peter Frendl <pfre00@gmail.com>
2017-03-07 13:22:24 +0100

    optimizer set to shared rmsprop

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     global_model.share_memory()
     local_model = ActorCritic(env.action_space.n)
     
-    optimizer = AsyncAdam(global_model.parameters(), local_model.parameters(), lr=args.lr)
+    optimizer = AsyncRMSprop(global_model.parameters(), local_model.parameters(), lr=args.lr)
 
     processes = []
     for rank in range(args.num_processes):
