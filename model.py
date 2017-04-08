c719d19bf36bee077f41350b4e42a9c790014db3
Peter Frendl <pfre00@gmail.com>
2017-04-08 23:50:16 +0200

    env wrapper added, train.py restructured, model now ikostrikov version (3x3 kernels)

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
 
-conv_out = 192
+conv_out = 32*4*3
 lstm_out = 256
 
 class ActorCritic(nn.Module):
@@ -10,9 +10,10 @@ class ActorCritic(nn.Module):
     def __init__(self, num_actions):
         super(ActorCritic, self).__init__()
         
-        self.conv1 = nn.Conv2d(3, 4, 8, stride=4, padding=3)
-        self.conv2 = nn.Conv2d(4, 8, 8, stride=4, padding=3)
-        self.conv3 = nn.Conv2d(8, 16, 6, stride=3, padding=2)
+        self.conv1 = nn.Conv2d(1, 32, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
         
         self.lstm = nn.LSTMCell(conv_out, lstm_out)
         
@@ -21,13 +22,11 @@ class ActorCritic(nn.Module):
         
         self.train()
     
-    def forward(self, inputs):
-        
-        x, (hx, cx) = inputs
-        
+    def forward(self, x, t, (hx, cx)):
         x = F.elu(self.conv1(x))
         x = F.elu(self.conv2(x))
         x = F.elu(self.conv3(x))
+        x = F.elu(self.conv4(x))
         x = x.view(-1, conv_out)
         
         hx, cx = self.lstm(x, (hx, cx))
--- train.py
+++ train.py
@@ -2,23 +2,13 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
-import torchvision
 
 import gym
 
 from datetime import datetime, timedelta
 
 from model import ActorCritic
-
-convert_state = torchvision.transforms.Compose([
-    torchvision.transforms.ToPILImage(),
-    #torchvision.transforms.Lambda(lambda x: x.convert('L')),
-    #torchvision.transforms.Scale(84),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Lambda(lambda x: x * 255),
-    #torchvision.transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)),
-    torchvision.transforms.Lambda(lambda x: x.unsqueeze(0)),
-])
+from envs import create_atari_env
 
 def ensure_shared_grads(model, shared_model):
     for param, shared_param in zip(model.parameters(), shared_model.parameters()):
@@ -32,20 +22,18 @@ def train(rank, args, shared_model, optimizer):
     
     t_start = datetime.now()
     
-    env = gym.make(args.env_name)
+    env = create_atari_env(args.env_name)
     env.seed(args.seed + rank)
     
     model = ActorCritic(env.action_space.n)
     
-    state = env.reset()
-    done = True
-
-    reward_sum = 0
     running_reward = 0
     episodes = 0
     
     if rank == 0:
         print("t_elapsed\tepisodes\trunning_reward\treward_sum")
+    
+    done = True
     
     while True:
         
@@ -54,6 +42,10 @@ def train(rank, args, shared_model, optimizer):
         if done:
             cx = Variable(torch.zeros(1, 256))
             hx = Variable(torch.zeros(1, 256))
+            state = env.reset()
+            episodes += 1
+            game_step = 0
+            reward_sum = 0
         else:
             cx = Variable(cx.data)
             hx = Variable(hx.data)
@@ -62,13 +54,13 @@ def train(rank, args, shared_model, optimizer):
         log_probs = []
         rewards = []
         entropies = []
-
+        
         for step in range(args.num_steps):
             
             if rank == 0 and args.render:
                 env.render()
             
-            value, logit, (hx, cx) = model((Variable(convert_state(state)), (hx, cx)))
+            value, logit, (hx, cx) = model(state, game_step, (hx, cx))
             prob = F.softmax(logit)
             log_prob = F.log_softmax(logit)
             entropy = -(log_prob * prob).sum(1)
@@ -84,23 +76,21 @@ def train(rank, args, shared_model, optimizer):
             entropies.append(entropy)
             
             reward_sum += reward
+            game_step += 1
             
             if done:
                 t_now = datetime.now()
                 t_elapsed = t_now - t_start
-                episodes += 1
                 running_reward = running_reward * 0.99 + reward_sum * 0.01
                 unbiased_running_reward = running_reward / (1 - pow(0.99, episodes))
                 if rank == 0:
                     print("{}\t{}\t{:.2f}\t{}".format(
                         t_elapsed, episodes, unbiased_running_reward, reward_sum))
-                reward_sum = 0
-                state = env.reset()
                 break
 
         R = torch.zeros(1, 1)
         if not done:
-            value, _, _ = model((Variable(convert_state(state)), (hx, cx)))
+            value, _, _ = model(state, game_step, (hx, cx))
             R = value.data
         R = Variable(R)
         
@@ -126,5 +116,6 @@ def train(rank, args, shared_model, optimizer):
             
         optimizer.zero_grad()
         (policy_loss + 0.5 * value_loss).backward()
+        torch.nn.utils.clip_grad_norm(model.parameters(), 40)
         ensure_shared_grads(model, shared_model)
         optimizer.step()
